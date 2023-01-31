4db056a110e19de728d55296d9e1be0d357485c4
alexbotha <alexbotha@hotmail.co.uk>
2023-01-31 15:49:06 +0000

    will add components

---
+++ src/components/RestuarantContainer.js
@@ -0,0 +1,9 @@
+import React from "react";
+function RestuarantContainer() {
+  return (
+    <React.Fragment>
+      <h1>hello</h1>
+    </React.Fragment>
+  );
+}
+export default RestuarantContainer;
---
+++ src/components/Review.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Review() {
+  return <h1>Review page </h1>;
+}
+export default Review;
