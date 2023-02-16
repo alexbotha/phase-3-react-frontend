30f08ae2979ec9a29d13b78c65d7b526cfe9887a
alexbotha <alexbotha@hotmail.co.uk>
2023-02-16 13:51:08 +0000

    will add navigate() to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+  let navigate = useNavigate();
   return (
     <React.Fragment>
       <div className="header">
-        <h2>Review My Restuarant</h2>
+        <h2 onClick={() => navigate("/")}>Review My Restuarant</h2>
 
-        <p className="underHeader">The home of local reviews</p>
+        <p className="underHeader">The home of restuarant reviews</p>
       </div>
     </React.Fragment>
   );
