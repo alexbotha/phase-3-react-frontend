194017d8a44a4ab9fff70eb96e16557ef8390cb8
alexbotha <alexbotha@hotmail.co.uk>
2023-02-16 13:56:04 +0000

    will add useLocation(), routes, & CB's for state

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import "../App.css";
 import Header from "./Header";
 import NavBar from "./NavBar";
@@ -8,10 +8,14 @@ import RestuarantContainer from "./RestuarantContainer";
 import Restuarant from "./Restuarant";
 import AddRestuarant from "./AddRestuarant";
 import ReviewContainer from "./ReviewContainer";
+import AddReview from "./AddReview";
+import WelcomePage from "./WelcomePage";
 
 function App() {
   const [restuarants, setRestuarants] = useState([]);
   const [reviews, setReviews] = useState([]);
+
+  let location = useLocation();
 
   useEffect(() => {
     fetch("http://localhost:9292/restuarants")
@@ -33,32 +37,63 @@ function App() {
     setRestuarants([...restuarants, newRestDetails]);
   }
 
+  function updatingReviewList(newReviewDetails) {
+    setReviews([...reviews, newReviewDetails]);
+  }
+
+  function deleteReview(deletedReviewDetails) {
+    const postDelete = reviews.filter((reviewObj) => {
+      return reviewObj.id !== deletedReviewDetails.id;
+    });
+    setReviews(postDelete);
+    fetch(`http://localhost:9292/reviews/${deletedReviewDetails.id}`, {
+      method: "DELETE",
+    });
+  }
+
   return (
-    <div className="app">
-      <Header />
-      <NavBar />
+    <>
+      {location.pathname === "/" ? (
+        <WelcomePage />
+      ) : (
+        <div className="app">
+          <Header />
+          <NavBar restuarants={restuarants} />
+          <Routes>
+            <Route
+              path="/restuarants"
+              element={<RestuarantContainer restuarants={restuarants} />}
+            />
+            <Route
+              path="/reviews"
+              element={
+                <ReviewContainer reviews={reviews} restuarants={restuarants} />
+              }
+            />
 
-      <Routes>
-        <Route
-          path="/restuarants"
-          element={<RestuarantContainer restuarants={restuarants} />}
-        />
-        <Route
-          path="/reviews"
-          element={<ReviewContainer reviews={reviews} />}
-        />
-
-        <Route path="/restuarants/:id" element={<Restuarant />} />
-        <Route path="/reviews/:id" element={<Review />} />
-        <Route
-          exact
-          path="/restuarants/new"
-          element={
-            <AddRestuarant updatingRestuarantList={updatingRestuarantList} />
-          }
-        />
-      </Routes>
-    </div>
+            <Route path="/restuarants/:id" element={<Restuarant />} />
+            <Route
+              path="/reviews/:id"
+              element={<Review deleteReview={deleteReview} />}
+            />
+            <Route
+              exact
+              path="/restuarants/new"
+              element={
+                <AddRestuarant
+                  updatingRestuarantList={updatingRestuarantList}
+                  restuarants={restuarants}
+                />
+              }
+            />
+            <Route
+              path="/reviews/new"
+              element={<AddReview updatingReviewList={updatingReviewList} />}
+            />
+          </Routes>
+        </div>
+      )}
+    </>
   );
 }
 
