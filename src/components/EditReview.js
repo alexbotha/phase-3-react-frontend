3c8055155b8494f8bb7c9a1b36e4666b27f5dc53
alexbotha <alexbotha@hotmail.co.uk>
2023-02-17 13:48:36 +0000

    will add useeffect to patch request component

--- src/components/EditReview.js
+++ src/components/EditReview.js
@@ -1,18 +1,27 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
-function EditReview({ editingReview }) {
+function EditReview() {
   let navigate = useNavigate();
   const { id } = useParams();
-  const [editReview, setEditReview] = useState({
+  const [review, setReview] = useState("");
+  const [details, setDetails] = useState({
     review: "",
     rating: "",
-    restuarant_id: "",
   });
 
+  useEffect(() => {
+    fetch(`http://localhost:9292/reviews/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setReview(data);
+        setDetails(data);
+      });
+  }, []);
+
   function handleChange(e) {
-    setEditReview({
-      ...editReview,
+    setDetails({
+      ...details,
       [e.target.name]: e.target.value,
     });
   }
@@ -24,30 +33,30 @@ function EditReview({ editingReview }) {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(editReview),
+      body: JSON.stringify(details),
     })
       .then((response) => response.json())
-      .then((editReviewDetails) => editingReview(editReviewDetails));
-    navigate("/reviews");
+      .then((editReviewDetails) => setReview(editReviewDetails));
+    navigate(`/reviews/${id}`);
   }
 
   return (
     <div>
-      <h3>Made a mistake? Correct your review now!</h3>
+      <h3 className="rest-details">{`Correct your review for ${review.company_name}`}</h3>
       <form className="form" onSubmit={handleSubmit}>
         <input
           type="text"
           name="review"
           onChange={handleChange}
           placeholder="Review"
-          value={editReview.review}
+          value={details.review}
         />
         <input
           type="text"
           name="rating"
           onChange={handleChange}
           placeholder="Rating"
-          value={editReview.rating}
+          value={details.rating}
         />
         <br />
         <br />
