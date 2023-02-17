27560894f80f9e6076366265f54d108643af7bb9
alexbotha <alexbotha@hotmail.co.uk>
2023-02-17 13:49:25 +0000

    styling update

--- src/components/AddRestuarantButton.js
+++ src/components/AddRestuarantButton.js
@@ -4,7 +4,10 @@ import { useNavigate } from "react-router-dom";
 function AddRestuarantButton() {
   let navigate = useNavigate();
   return (
-    <button onClick={() => navigate("/restuarants/new")}>
+    <button
+      className="add-res-btn"
+      onClick={() => navigate("/restuarants/new")}
+    >
       Add new restuarant
     </button>
   );
