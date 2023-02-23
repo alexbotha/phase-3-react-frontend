ded9bb0d8dabe2b03494730819c8ad3ddd2f497f
alexbotha <alexbotha@hotmail.co.uk>
2023-02-23 14:38:28 +0000

    will add mapping to a variable for readability

--- src/components/ReviewContainer.js
+++ src/components/ReviewContainer.js
@@ -3,6 +3,9 @@ import ReviewItem from "./ReviewItem";
 import AddReviewButton from "./AddReviewButton";
 
 function ReviewContainer({ reviews }) {
+  const review = reviews.map((review) => (
+    <ReviewItem key={review.id} id={review.id} review={review} />
+  ));
   return (
     <>
       <div className="review-heading">
@@ -10,9 +13,7 @@ function ReviewContainer({ reviews }) {
         <h2>Review</h2>
         <h2>Rating</h2>
       </div>
-      {reviews.map((review) => (
-        <ReviewItem key={review.id} id={review.id} review={review} />
-      ))}
+      {review}
       <br></br>
       <hr></hr>
       <AddReviewButton />
