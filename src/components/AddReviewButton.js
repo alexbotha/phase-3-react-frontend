570853740ea8c53607d0dc0cb1fd94dba6cc744e
alexbotha <alexbotha@hotmail.co.uk>
2023-03-06 13:00:25 +0000

    review only added through association

--- src/components/AddReviewButton.js
+++ src/components/AddReviewButton.js
@@ -2,12 +2,12 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 function AddReviewButton() {
-  let navigate = useNavigate();
-  return (
-    <button onClick={() => navigate("/reviews/new")}>
-      Click to add a new review
-    </button>
-  );
+  // let navigate = useNavigate();
+  // return (
+  // <button onClick={() => navigate("reviews/new")}>
+  //   Click to add a new review
+  // </button>
+  // );
 }
 
 export default AddReviewButton;
